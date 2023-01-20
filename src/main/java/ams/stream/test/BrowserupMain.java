4ccf61377d02eeb2524e0d222001aa84971fbd94
WilhelmStu <willi.stuhle@gmail.com>
2023-01-20 11:57:01 +0100

    Initial buffer detection and output, Proxy/Rest does not work

---
+++ src/main/java/ams/stream/test/BrowserupMain.java
@@ -0,0 +1,30 @@
+package ams.stream.test;
+
+// Import classes:
+import com.browserup.proxy_client.ApiClient;
+import com.browserup.proxy_client.ApiException;
+import com.browserup.proxy_client.Configuration;
+import com.browserup.proxy_client.Counter;
+import com.browserup.proxy.api.BrowserUpProxyApi;
+
+/**
+ * Does not work
+ */
+public class BrowserupMain {
+    public static void main(String[] args) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("http://localhost:8088");
+
+        BrowserUpProxyApi apiInstance = new BrowserUpProxyApi(defaultClient);
+        Counter counter = new Counter(); // Counter | Receives a new counter to add. The counter is stored, under the hood, in an array in the har under the _counters key
+        try {
+            apiInstance.addCounter(counter);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling BrowserUpProxyApi#addCounter");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
--- src/test/java/ams/stream/test/MainPageTest.java
+++ src/test/java/ams/stream/test/MainPageTest.java
@@ -1,5 +1,7 @@
 package ams.stream.test;
 
+import com.browserup.proxy.api.BrowserUpProxyApi;
+import com.browserup.proxy_client.ApiException;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -7,11 +9,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chromium.ChromiumNetworkConditions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v102.network.Network;
+import org.openqa.selenium.devtools.v102.network.model.Headers;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,32 +38,84 @@ import static com.codeborne.selenide.Selenide.*;
 public class MainPageTest {
     //MainPage mainPage = new MainPage();
     private ChromeDriver driver;
+    private static final Integer PROXY_PORT = 8080;
 
     @BeforeAll
-    public static void setUpAll() {
+    public static void setUpAll() throws IOException {
         Configuration.browserSize = "1920x1080";
-        SelenideLogger.addListener("allure", new AllureSelenide());
+        //Process p = Runtime.getRuntime().exec("mitmdump -s script2.py > out2.txt");
+        //new Thread((Runnable) p.);// todo
+        //SelenideLogger.addListener("allure", new AllureSelenide());
+        /*
+        try {
+            new BrowserUpProxyApi().resetHarLog();
+        }catch (ApiException e){
+            e.printStackTrace();
+        }
+
+         */
     }
 
     @BeforeEach
     public void setUp() {
         ChromeDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--no-sandbox");
-        //options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--headless");
+        options.addArguments("--proxy-server=http://localhost:" + PROXY_PORT);
+        /*
+        var options = new ChromeOptions().addArguments(
+                //"--headless",
+                "--disable-extensions",
+                "--proxy-server=http://localhost:" + PROXY_PORT
+                );
 
+         */
         driver = new ChromeDriver(options);
     }
 
+    // detect buffering: https://stackoverflow.com/questions/21399872/how-to-detect-whether-html5-video-has-paused-for-buffering
+    // video element reference: https://www.w3schools.com/tags/ref_av_dom.asp
+    // performance of browser : https://stackoverflow.com/questions/45847035/using-selenium-how-to-get-network-request
+    // proxy: https://github.com/browserup/mitmproxy/blob/main/clients/examples/java/src/test/java/com/javatest/JavaClientTest.java
     @Test
-    public void test1(){
+    public void test1() {
+
         driver.get("https://streaming.stulpinger.at/");
         driver.setNetworkConditions(new ChromiumNetworkConditions()); //todo
+        WebElement video = driver.findElement(By.tagName("video"));
+        StringBuilder build = new StringBuilder();
         try {
-            Thread.sleep(10000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+        while (true) {
+            build.setLength(0);
+            JavascriptExecutor js = driver;
+
+            double bufferUpperBound = 0;
+            double currentTime = 0;
+            try {
+                bufferUpperBound = Double.parseDouble((String) js.executeScript("return JSON.stringify(arguments[0].buffered.end(0));", video));
+                currentTime = Double.parseDouble((String) js.executeScript("return JSON.stringify(arguments[0].currentTime);", video));// time
+            }catch (Exception e){
+                System.out.println("Buffer of 0 / Error");
+            }
+
+            build.append(new Timestamp(System.currentTimeMillis())).append("; ");
+            double bufferSize = bufferUpperBound - currentTime;
+
+            if (bufferSize < 0.05) {
+                build.append("0.0; buffering");
+            } else {
+                build.append(bufferSize);
+            }
+
+            System.out.println(build);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
     }
